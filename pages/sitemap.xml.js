4d06e285a76ceb795b658b1a3e2827ce170b203e
nchimbit <ismailnchimbi@gmail.com>
2026-06-08 09:43:59 +0000

    fix: sitemap route

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,14 +1,20 @@
 import { getAllPosts } from "../lib/posts";
-const SITE = "https://migrantscholar.com";
+
+const SITE = "https://migrantscholar.vercel.app";
+
 function generateSitemap(posts) {
   return `<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>${SITE}/</loc><changefreq>daily</changefreq><priority>1.0</priority></url><url><loc>${SITE}/blog</loc><changefreq>daily</changefreq><priority>0.9</priority></url>${posts.map(({slug,date})=>`<url><loc>${SITE}/blog/${slug}</loc><lastmod>${date}</lastmod><changefreq>monthly</changefreq><priority>0.7</priority></url>`).join("")}</urlset>`;
 }
-export default function Sitemap() {}
-export async function getServerSideProps({res}) {
+
+export default function Sitemap() {
+  return null;
+}
+
+export async function getServerSideProps({ res }) {
   const posts = getAllPosts();
-  res.setHeader("Content-Type","text/xml");
-  res.setHeader("Cache-Control","public, s-maxage=3600");
+  res.setHeader("Content-Type", "text/xml");
+  res.setHeader("Cache-Control", "public, s-maxage=3600");
   res.write(generateSitemap(posts));
   res.end();
-  return {props:{}};
+  return { props: {} };
 }
