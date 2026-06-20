15266eb1dd1addbbf0729182d4e31924222c6cb0
nchimbit <ismailnchimbi@gmail.com>
2026-06-20 07:18:27 +0000

    feat: add teal globe favicon

---
+++ pages/_document.js
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.svg" />
+        <meta name="theme-color" content="#0D6E6E" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
