ec90ffd5049ef618640cefd4cd91122b4ca11138
nchimbit <ismailnchimbi@gmail.com>
2026-06-22 08:33:54 +0000

    feat: add Google Search Console verification tag

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="google-site-verification" content="xD-MIsy__wA8eoQYHgCK35xdAk0LjbPAxl2sDxAQifg" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/png" href="/favicon.svg" />
         <meta name="theme-color" content="#0D6E6E" />
