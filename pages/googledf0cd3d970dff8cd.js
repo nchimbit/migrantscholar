fe34d89ba13dbe2446944b4941b6cb76c474c323
nchimbit <ismailnchimbi@gmail.com>
2026-06-22 10:16:08 +0000

    fix: GSC verification file with correct content format

--- pages/googledf0cd3d970dff8cd.js
+++ pages/googledf0cd3d970dff8cd.js
@@ -1,4 +1,10 @@
-import React from 'react'
 export default function GSCVerification() {
-  return React.createElement('p', null, 'google-site-verification: xD-MIsy__wA8eoQYHgCK35xdAk0LjbPAxl2sDxAQifg')
+  return null;
+}
+
+export async function getServerSideProps({ res }) {
+  res.setHeader('Content-Type', 'text/html');
+  res.write('google-site-verification: xD-MIsy__wA8eoQYHgCK35xdAk0LjbPAxl2sDxAQifg');
+  res.end();
+  return { props: {} };
 }
