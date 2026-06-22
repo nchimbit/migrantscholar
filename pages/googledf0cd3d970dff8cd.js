ed0cba5021e2fba6d1b022d7162e17e7e0a0d7b7
nchimbit <ismailnchimbi@gmail.com>
2026-06-22 10:20:01 +0000

    fix: GSC verification with proper HTML format

--- pages/googledf0cd3d970dff8cd.js
+++ pages/googledf0cd3d970dff8cd.js
@@ -4,7 +4,7 @@ export default function GSCVerification() {
 
 export async function getServerSideProps({ res }) {
   res.setHeader('Content-Type', 'text/html');
-  res.write('google-site-verification: xD-MIsy__wA8eoQYHgCK35xdAk0LjbPAxl2sDxAQifg');
+  res.write(`<!DOCTYPE html><html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>Google Search Console</title></head><body>google-site-verification: xD-MIsy__wA8eoQYHgCK35xdAk0LjbPAxl2sDxAQifg</body></html>`);
   res.end();
   return { props: {} };
 }
